b957c270de4f93cf68792a18f3e004c866240e9b
Adrián Bíro <biroadrian@protonmail.com>
2022-11-03 16:51:06 +0100

    small fixes

---
+++ ,checkssl.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+set -o errexit
+set -o nounset
+set -o pipefail
+data=$(echo \
+  | openssl s_client -servername ${1} -connect ${1}:443 2>/dev/null \
+  | openssl x509 -noout -enddate \
+  | sed -e 's#notAfter=##')
+
+ssldate=$(date -d "${data}" '+%s')
+nowdate=$(date '+%s')
+diff="$((${ssldate}-${nowdate}))"
+
+echo $((${diff}/86400))
