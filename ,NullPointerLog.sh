01aff11cce6e831d6071924786da2c233917d8eb
Adrián Bíro <89740062+adrianbiro@users.noreply.github.com>
2023-03-24 11:42:55 +0100

    Update ,NullPointerLog.sh
    filter full log

--- ,NullPointerLog.sh
+++ ,NullPointerLog.sh
@@ -2,4 +2,5 @@
 set -o errexit
 set -o nounset
 set -o pipefail
-awk '/java.lang.NullPointerException/,/^[0-3]?[0-9].[0-3]?[0-9].(?:[0-9]{2})?[0-9]{2}.*$/' ${1:?"Usage: ${0} <logfile>"}
+echo -e ""$(hostname)":$(readlink -f "${*}" | sed "s|${HOME}||g")\n"
+awk '/^[0-3]?[0-9].[0-3]?[0-9].(?:[0-9]{2})?[0-9]{2}.*$/{print "\n" $0; next}{print}' "${@}"  | awk 'BEGIN{RS=""}/java.lang.NullPointerException/'
