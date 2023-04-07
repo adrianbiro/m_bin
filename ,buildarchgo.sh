e4e21c57a98af4ad0bc212e93d376cdfc1cb684e
Adrian Biro <biroadrian@protonmail.com>
2023-04-07 13:36:18 +0200

    small fixes

--- ,buildarchgo.sh
+++ ,buildarchgo.sh
@@ -1,25 +1,23 @@
 #!/bin/bash
-package=$1
-if [[ -z "$package" ]]; then
-    echo "usage: $0 <package-name>"
-    exit 1
-fi
-package_split=(${package//\// })
-package_name=${package_split[-1]}
-# go tool dist list
+PACKAGE="${1:? usage: ${0##*/} <package-name>}"
+
+PACKAGE_split=("${PACKAGE//\// }")
+PACKAGE_name=${PACKAGE_split[-1]}
+
+mkdir "bin" 2>"/dev/null"
+
 platforms=("windows/amd64" "linux/amd64")
 
 for platform in "${platforms[@]}"; do
-    platform_split=(${platform//\// })
-    GOOS=${platform_split[0]}
-    GOARCH=${platform_split[1]}
-    output_name=$package_name'-'$GOOS'-'$GOARCH
-    if [ $GOOS = "windows" ]; then
+    #for platform in $(go tool dist list); do
+    GOOS="${platform%%/*}"
+    GOARCH="${platform##*/}"
+    output_name="bin/${PACKAGE_name}-${GOOS}-${GOARCH}"
+    if [[ "${GOOS}" = "windows" ]]; then
         output_name+='.exe'
     fi
 
-    env GOOS=$GOOS GOARCH=$GOARCH go build -o $output_name $package
-    if [ $? -ne 0 ]; then
+    if ! env GOOS="${GOOS}" GOARCH="${GOARCH}" go build -o "${output_name}" "${PACKAGE}"; then
         echo 'An error has occurred! Aborting the script execution...'
         exit 1
     fi
