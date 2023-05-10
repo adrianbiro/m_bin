d05138574ea546c0141a82fc71f4cba10a2b9516
Adrián Bíro <89740062+adrianbiro@users.noreply.github.com>
2023-05-10 11:19:56 +0200

    Create ,shiftlines.sh

---
+++ mix/,shiftlines.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+if [[ ${#} -eq 0 ]]; then echo -e "Usage:\n\t${0} <input_file> <output_file> <number_lines_to_shift>"; exit 1; fi
+infile=${1:?}
+outfile=${2:?}
+numlines=${3:?}
+
+# add header
+head -n 1 "${infile}" >>"${outfile}"
+# skip header shift first N lines 2-n
+sed -i -e 2,${numlines}$'{w/dev/stdout\n;d}' "${infile}" >>"${outfile}"
+
+doc=:'
+ls -lt /app/csops/import/COLI.CSCOLL.SMAPP1.CI*
+## COLI.CSCOLL.SMAPP1.CI230424.A001.PROC
+## COLI.CSCOLL.SMAPP1.CI230424.A001.TODO
+#####
+## in pod
+mkdir -p ~/bin; cat > ~/bin/,shiftlines.sh
+## ctrl+d
+chmod +x ~/bin/,shiftlines.sh
+export PATH=$HOME/bin:$PATH
+'
