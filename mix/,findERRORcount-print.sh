f5a9f115117bf9d60eac13383b07ff950b4663de
Adrián Bíro <89740062+adrianbiro@users.noreply.github.com>
2023-05-17 20:41:27 +0200

    Update ,findERRORcount-print.sh

--- mix/,findERRORcount-print.sh
+++ mix/,findERRORcount-print.sh
@@ -4,4 +4,4 @@
 find /srv/log/xp/openshift/appdata/smart-prod-applogs -newermt "$(date -d "2 day ago" +"%Y-%m-%d")" ! -newermt "$(date +"%Y-%m-%d")"  -name "burda-*" -exec zgrep -c ERROR {} \; | awk  '{sum+=int($0)}END{print sum}'
 
 # print log file name, plus print errors with ../,parserLog4j.py  
-find /srv/log/xp/openshift/appdata/smart-prod-applogs -newermt "$(date -d "2 day ago" +"%Y-%m-%d")" ! -newermt "$(date +"%Y-%m-%d")" -name "burda-*" -print0 | while IFS= read -r -d '' file; do if "$(zgrep -q ERROR "${file}")"; then  echo "${file}"; ~/bin/,parseLog4j.py -e 'ERROR' "${file}"; fi; done
+find /srv/log/xp/openshift/appdata/smart-prod-applogs -newermt "$(date -d "2 day ago" +"%Y-%m-%d")" ! -newermt "$(date +"%Y-%m-%d")" -name "burda-*" -print0 | while IFS= read -r -d '' file; do if "$(zgrep -qm 1 ERROR "${file}")"; then  echo "${file}"; ~/bin/,parseLog4j.py -e 'ERROR' "${file}"; fi; done
