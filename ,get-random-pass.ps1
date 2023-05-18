fc6939552afe465a293d932df2718df94c770489
Adrián Bíro <89740062+adrianbiro@users.noreply.github.com>
2023-05-18 14:36:30 +0200

    Update ,get-random-pass.ps1

--- ,get-random-pass.ps1
+++ ,get-random-pass.ps1
@@ -5,3 +5,5 @@ Param(
 [string[]] $ASCII = (33..126 | ForEach-Object { [char]$_ })  
 
 Write-Host ( -Join (Get-Random -InputObject $ASCII -Count $Length), [string]::Empty).Trim()
+
+#( -Join (Get-Random -InputObject  (65..122 | ForEach-Object { if (-not (91..96 -contains $_)){[char]$_ }}) -Count 30), [string]::Empty).Trim() | Set-Clipboard; Get-Clipboard
