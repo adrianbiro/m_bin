60688e10affe0c1e9e96b30aa135179d1024d759
Adrian Biro <biroadrian@protonmail.com>
2023-06-27 20:07:20 +0200

    small fixes

---
+++ mix/_install.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+function _sudo() {
+    if [[ "0" == "$(id --user)" ]]; then
+        "$@"
+    else
+        sudo "$@"
+    fi
+}
+_sudo find /opt/bin -maxdepth 1 ! -name "${0##*/}" -type f -executable -exec ln -sfv {} /usr/local/bin/ \;
