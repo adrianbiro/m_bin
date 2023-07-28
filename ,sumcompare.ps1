c5ee0efbba098adae9051ac3e8ab70b274f21a1e
Adrian Biro <biroadrian@protonmail.com>
2023-07-28 18:50:22 +0200

    small fixes

---
+++ ,sumcompare.ps1
@@ -0,0 +1,40 @@
+#!/usr/bin/pwsh
+<#
+    .SYNOPSIS
+        Check if checksum is valid
+
+    .DESCRIPTION
+
+    .NOTES
+        This PowerShell script compare sha256sum.
+        You need to specify file as first argument,
+        and copy checksum for reference to clipboard.
+    .LINK
+        https://github.com/adambiro1/winbin
+        https://github.com/adrianbiro/winbin
+#>
+param([string]$File)
+if($File) {
+    if (-not (Test-Path -Path $File -PathType Leaf)){
+        "$File does not exist."
+        exit
+    }
+    $checksum = Get-Clipboard
+    $valid = foreach($i in [char]'A'..[char]'F' && 0..9) {$i}
+    $ascii = foreach($i in 33..126) {"$([char]$i)"}
+    foreach($i in $ascii) {
+        if($valid -contains $i) { continue }
+        if($checksum.ToUpper().ToCharArray() -contains $i) {
+            "Checksum in clipboard is not valid"
+            exit
+        }
+    }
+    $isoHash = Get-FileHash $File -Algorithm sha256
+    if ($isoHash.Hash -eq $checksum.ToUpper()) {
+        "Checksum is valid."
+    } else {
+        "Checksum is not valid"
+    }
+} else {
+    "Usage:`n`t{0} <file>" -f $MyInvocation.MyCommand.Name
+}
---
+++ mix/escapeXMLchars.sed
@@ -0,0 +1,15 @@
+#!/usr/bin/sed -Ef
+#https://www.ibm.com/docs/en/was-liberty/base?topic=SSEQTP_liberty%2Fcom.ibm.websphere.wlp.doc%2Fae%2Frwlp_xml_escape.htm
+
+s/"/&quot;/g
+
+s/'/&apos;/g
+
+s/</&lt;/g
+
+s/>/&gt;/g
+
+#https://stackoverflow.com/a/25273781/18172103
+#s/\&/&amp;amp;/g
+
+s/\&/&amp;/g
