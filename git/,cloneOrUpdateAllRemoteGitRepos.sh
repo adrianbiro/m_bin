bb38754c6e61053e6b6bf36e91c5a100d62cde28
Adrian Biro <biroadrian@protonmail.com>
2023-08-06 17:48:28 +0200

    small fixes

--- git/,cloneOrUpdateAllRemoteGitRepos.sh
+++ git/,cloneOrUpdateAllRemoteGitRepos.sh
@@ -8,40 +8,29 @@ NAME='adrianbiro'
 OLDPWD="${PWD}"
 GITSDIR="${HOME}/allgits"
 readonly TOKEN NAME GITSDIR
-#TOUPDATE=()
 
 function gitstuff {
-  local gitrepo dirnamedotgit dirname
-  gitrepo="${1:?}"                # git@github.com:adrianbiro/zfs_exporter.git
-  dirnamedotgit="${gitrepo##*/}"  # zfs_exporter.git
-  dirname="${dirnamedotgit%.git}" # zfs_exporter
+  local gitrepo
+  gitrepo="${1:?}" # git@github.com:adrianbiro/zfs_exporter.git
   cd "${GITSDIR}" >"/dev/null" || return
-  # if there is repo update it #TODO update
-  #if echo "${TOUPDATE[*]}" | grep -q "${dirname}"; then
-  #  cd "${GITSDIR}/${dirname}" #|| return
-  #  git fetch --all
-  #else
+  if [[ -d ".git" ]]; then
+    git fetch --all --tags
+  else
+    git clone --mirror "${gitrepo}"
+  fi
   : <<'END_COMMENT'
 https://docs.github.com/en/rest/repos/repos?apiVersion=2022-11-28#list-repositories-for-the-authenticated-user
 https://archive.kernel.org/oldwiki/git.wiki.kernel.org/index.php/Git_FAQ.html#How_do_I_clone_a_repository_with_all_remotely_tracked_branches.3F
 https://docs.github.com/en/authentication/keeping-your-account-and-data-secure/creating-a-personal-access-token
 END_COMMENT
-  #if [[ $(ls -A | wc -l) -eq 0 ]]; then
-  git clone --mirror "${gitrepo}" "${dirname}/.git"
-  cd "${GITSDIR}/${dirname}" || return
-  git config --bool core.bare false && git checkout "$(git branch --show-current)" >"/dev/null"
-  # fi
 }
 function main {
   if [[ ! -d "${GITSDIR}" ]]; then
     mkdir "${GITSDIR}"
   fi
 
-  #mapfile -d $'\0' TOUPDATE < <(find "${GITSDIR}" -type d -name ".git" -prune -print0)
-  
   local page
   declare -i page=1
-  #page=1
   while :; do
     json=$(
       curl -s "https://api.github.com/users/$NAME/repos" -G \
@@ -58,6 +47,6 @@ function main {
       gitstuff "${line}"
     done
   done
-  cd "${OLDPWD}" #|| return
+  cd "${OLDPWD}" || return
 }
 main "${@}"
