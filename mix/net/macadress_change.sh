9a0ddf94bc1497dae61ce41721e73d1ff3f7e7de
Adrian Biro <biroadrian@protonmail.com>
2023-12-17 14:54:08 +0100

    small fixes

--- mix/net/macadress_change.sh
+++ mix/net/macadress_change.sh
@@ -2,8 +2,8 @@
 
 ifconfig | awk -F':' '/=/{print $1}' | xargs -I% sh -c 'echo "Interface: %"
     ifconfig % down
-    #maccanger -a --another  Set random vendor MAC of the same kind.
-    macchanger -rb % # --random --bia  When setting fully random MAC pretend to be a burned-in-address. If not used, the MAC will have the locally-administered bit set.
-    #maccanger -p  # --permanent  Reset MAC address to its original, permanent hardware value.
+    #maccanger -a %   #--another  Set random vendor MAC of the same kind.
+    macchanger -rb %  # --random --bia  When setting fully random MAC pretend to be a burned-in-address. If not used, the MAC will have the locally-administered bit set.
+    #maccanger -p %   # --permanent  Reset MAC address to its original, permanent hardware value.
     ifconfig % up
     echo ""'
