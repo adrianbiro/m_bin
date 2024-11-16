62c98b1399296dedaf241766df1cdc067644cc96
Adrian Biro <biroadrian@protonmail.com>
2024-11-16 20:51:25 +0100

    add sudo safe unmount

--- ,safe_unmount.sh
+++ ,safe_unmount.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 device="${1:?${0##*/} </dev/sdb1>}"
-udisksctl unmount --block-device "${device}"
-udisksctl power-off --block-device "${device}"
+sudo udisksctl unmount --block-device "${device}"
+sudo udisksctl power-off --block-device "${device}"
